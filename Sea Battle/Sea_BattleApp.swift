7c0fcf73756f096e91d777b90c5afe9b074140d1
Matt Novoselov <matveynovoselov@gmail.com>
2023-11-30 09:05:50 +0100

    Added blank xcode project

---
+++ Sea Battle/Sea_BattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sea_BattleApp.swift
+//  Sea Battle
+//
+//  Created by Matt Novoselov on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Sea_BattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
